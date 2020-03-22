700ec61366e016438feb4daf09eadcd94ef810b6
Aaron Atkins <aaron.atkins91@gmail.com>
2020-03-22 21:37:39 +1100

    2.4: Course contents step9

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom'
 
 const Course = ({ course }) => {
   return (
-    <>
+    <div>
       <Header course={course} />
       <Content course={course} />
-    </>
+    </div>
   )
 }
 
 const Header = ({ course }) => {
   return (
     <div>
-      <h1>{course.name}</h1>
+      <h2>{course.name}</h2>
     </div>
   )
 }
@@ -40,31 +40,57 @@ const Part = ({ part, exercises}) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <>
-      <Course course={course} />
+      <div>
+        <h1>Web development curriculum</h1>
+      </div>
+      {courses.map( course => <Course key={course.id} course={course} />)}
     </>
   )
 }
