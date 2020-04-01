d7146b9a5a0666489bde6b4eb679b2842b47fc66
Aaron Atkins <aaron.atkins91@gmail.com>
2020-04-02 09:36:57 +1100

    4.5*: helper functions and unit tests, step3

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -6,6 +6,24 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
 };
 
+const favouriteBlog = (blogs) => {
+  const likes = [...blogs.map((blog) => blog.likes)];
+
+  if (likes.length === 0) {
+    return undefined;
+  }
+
+  const highestLikes = Math.max(...likes);
+  const highestLikedBlog = blogs.find((blog) => blog.likes === highestLikes);
+
+  return {
+    title: highestLikedBlog.title,
+    author: highestLikedBlog.author,
+    likes: highestLikedBlog.likes
+  };
+};
+
 module.exports = {
-  totalLikes
+  totalLikes,
+  favouriteBlog
 };
