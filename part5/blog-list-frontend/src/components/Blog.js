68471f753846191d08cff40bb71d1c3e70dce00f
Aaron Atkins <aaron.atkins91@gmail.com>
2020-04-06 20:17:53 +1000

    5.7* Blog list frontend, step7

--- part5/blog-list-frontend/src/components/Blog.js
+++ part5/blog-list-frontend/src/components/Blog.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Blog = ({ blog }) => (
-  <div>
-    "{blog.title}" by {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [showFullBlog, setShowFullBlog] = useState(false);
+
+  const toggleVisibility = () => {
+    setShowFullBlog(!showFullBlog);
+  };
+
+  const blogStyle = {
+    padding: 10,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 10,
+    color: 'white'
+  }
+
+  if (!showFullBlog) {
+    return (
+      <div style={blogStyle}>
+        "{blog.title}" by {blog.author}{' '}
+        <button type="button" onClick={toggleVisibility}>View</button>
+      </div>
+    );
+  }
+
+  return (
+    <div onClick={toggleVisibility} style={blogStyle}>
+      <div>
+        "{blog.title}" by {blog.author}{' '}
+        <button type="button" onClick={toggleVisibility}>Hide</button>
+      </div>
+      <div>
+        {blog.url}
+      </div>
+      <div>
+        {blog.likes}{' '}
+        <button type="button">Like</button>
+      </div>
+      <div>
+        {blog.user.name}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
