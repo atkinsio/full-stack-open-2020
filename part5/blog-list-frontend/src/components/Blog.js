74e46d5d1326465394c5a5ae32ad1ffe6b9a4be8
Aaron Atkins <aaron.atkins91@gmail.com>
2020-04-06 20:33:28 +1000

    5.8*: Blog list frontend, step7

--- part5/blog-list-frontend/src/components/Blog.js
+++ part5/blog-list-frontend/src/components/Blog.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import blogService from '../services/blogs';
 
 const Blog = ({ blog }) => {
   const [showFullBlog, setShowFullBlog] = useState(false);
+  const [likes, setLikes] = useState(blog.likes);
 
   const toggleVisibility = () => {
     setShowFullBlog(!showFullBlog);
@@ -13,13 +15,30 @@ const Blog = ({ blog }) => {
     borderWidth: 1,
     marginBottom: 10,
     color: 'white'
-  }
+  };
+
+  const increaseLikes = async () => {
+    const newBlog = {
+      user: blog.user._id,
+      likes: blog.likes,
+      author: blog.author,
+      title: blog.titile,
+      url: blog.url
+    };
+
+    newBlog.likes = newBlog.likes + 1;
+
+    const returnedBlog = await blogService.update(blog.id, newBlog);
+    setLikes(returnedBlog.likes)
+  };
 
   if (!showFullBlog) {
     return (
       <div style={blogStyle}>
         "{blog.title}" by {blog.author}{' '}
-        <button type="button" onClick={toggleVisibility}>View</button>
+        <button type="button" onClick={toggleVisibility}>
+          View
+        </button>
       </div>
     );
   }
@@ -28,18 +47,15 @@ const Blog = ({ blog }) => {
     <div onClick={toggleVisibility} style={blogStyle}>
       <div>
         "{blog.title}" by {blog.author}{' '}
-        <button type="button" onClick={toggleVisibility}>Hide</button>
+        <button type="button" onClick={toggleVisibility}>
+          Hide
+        </button>
       </div>
+      <div>{blog.url}</div>
       <div>
-        {blog.url}
+        {likes} <button type="button" onClick={increaseLikes}>Like</button>
       </div>
-      <div>
-        {blog.likes}{' '}
-        <button type="button">Like</button>
-      </div>
-      <div>
-        {blog.user.name}
-      </div>
+      <div>{blog.user.name}</div>
     </div>
   );
 };
